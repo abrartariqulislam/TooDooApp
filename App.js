27bd0771a21f241af5b7bc708625efcd9b0b617b
abrartariqulislam <islamtariqul010@gmail.com>
2023-08-02 20:06:01 +0600

    finishid todo app

--- App.js
+++ App.js
@@ -29,5 +29,27 @@ addBtn.addEventListener("click", function (e){
       addTask(inputText)
    }
 })
+TaskContainer.addEventListener("click" , function(e) {
+   const targetElement = e.target
+   if (targetElement.className === "delete"){
+      targetElement.parentElement.parentElement.remove()
+   }
+   if (targetElement.className === "status"){
+      targetElement.parentElement.previousElementSibling.classList.toggle("changeStatus")
+   }
+   if (targetElement.className === "edit"){
+      const newElement = targetElement.parentElement.parentElement.firstElementChild
+      const newInput =  document.createElement("input");
+      newInput.addEventListener("keypress" , function (e){
+         const newElementValue = e.target.value
+         if (newElementValue && e.key === "Enter") {
+            newElement.innerText = newElementValue
+         }
+      })
+      newInput.value = newElement.textContent;
+      newElement.innerHTML = "";
+      newElement.appendChild(newInput);
 
+   }
+})
 
